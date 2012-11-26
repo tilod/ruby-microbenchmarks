440ae989eee5a32ee74b19ab2970e77cea06ea58
Tilo Dietrich <tandrokor@gmail.com>
2012-11-26 23:54:06 +0100

    tweaking direct_assignment vs. merge

--- direct_assingment_vs_merge.rb
+++ direct_assingment_vs_merge.rb
@@ -2,67 +2,22 @@ require 'benchmark'
 
 random_ary = (1..1000).map { rand(1..100) }
 
+@offset = 0
+def useless_hash(entries)
+  hash = Hash[(@offset..@offset + entries).map { |i| [i, rand(1..100)] }]
+  @offset += entries
+
+  hash
+end
+
+
 Benchmark.bm(40) do |bm|
 
   puts 'Hash: direct assignment vs. merge'
   puts '---------------------------------'
   puts
 
-  bm.report '    direkt assignment (10 entries):' do
-    100_000.times do
-      hash = {}
-      10.times do |i|
-        hash[i] = random_ary[i]
-      end
-    end
-  end
-
-  bm.report '    merge (10 entries):' do
-    100_000.times do
-      hash = {}
-      10.times do |i|
-        hash = hash.merge(i => random_ary[i])
-      end
-    end
-  end
-
-  bm.report '    merge! (10 entries):' do
-    100_000.times do
-      hash = {}
-      10.times do |i|
-        hash.merge!(i => random_ary[i])
-      end
-    end
-  end
-
-  bm.report '    direkt assignment (100 entries):' do
-    10_000.times do
-      hash = {}
-      100.times do |i|
-        hash[i] = random_ary[i]
-      end
-    end
-  end
-
-  bm.report '    merge (100 entries):' do
-    10_000.times do
-      hash = {}
-      100.times do |i|
-        hash = hash.merge(i => random_ary[i])
-      end
-    end
-  end
-
-  bm.report '    merge! (100 entries):' do
-    10_000.times do
-      hash = {}
-      100.times do |i|
-        hash.merge!(i => random_ary[i])
-      end
-    end
-  end
-
-  bm.report '    direkt assignment (1000 entries):' do
+  bm.report '    direct assignment (1000x1):' do
     1000.times do
       hash = {}
       1000.times do |i|
@@ -71,7 +26,16 @@ Benchmark.bm(40) do |bm|
     end
   end
 
-  bm.report 'merge (1000 entries):' do
+  bm.report '    merge! (1000x1):' do
+    1000.times do
+      hash = {}
+      1000.times do |i|
+        hash.merge!(i => random_ary[i])
+      end
+    end
+  end
+
+  bm.report '    merge (1000x1):' do
     1000.times do
       hash = {}
       1000.times do |i|
@@ -80,11 +44,64 @@ Benchmark.bm(40) do |bm|
     end
   end
 
-  bm.report '    merge! (1000 entries):' do
+  hashes = (1..100).map{useless_hash(10)}
+  
+  bm.report '    direct assignment (100x10):' do
     1000.times do
       hash = {}
-      1000.times do |i|
-        hash.merge!(i => random_ary[i])
+      hashes.each do |h|
+        h.each do |k, v|
+          hash[k] = v
+        end
+      end
+    end
+  end
+
+  bm.report '    merge! (100x10):' do
+    1000.times do
+      hash = {}
+      hashes.each do |h|
+        hash.merge!(h)
+      end
+    end
+  end
+
+  bm.report '    merge (100x10):' do
+    1000.times do
+      hash = {}
+      hashes.each do |h|
+        hash = hash.merge(h)
+      end
+    end
+  end
+
+  hashes = (1..10).map{useless_hash(100)}
+
+  bm.report '    direct assignment (10x100):' do
+    1000.times do
+      hash = {}
+      hashes.each do |h|
+        h.each do |k, v|
+          hash[k] = v
+        end
+      end
+    end
+  end
+
+  bm.report '    merge! (10x100):' do
+    1000.times do
+      hash = {}
+      hashes.each do |h|
+        hash.merge!(h)
+      end
+    end
+  end
+
+  bm.report '    merge (10x100):' do
+    1000.times do
+      hash = {}
+      hashes.each do |h|
+        hash = hash.merge(h)
       end
     end
   end
